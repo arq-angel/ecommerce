e9399aab58c114f901b109b3c0e1f52d7a45e327
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-09-22 18:11:03 +1000

    complete section 48 - ecommerce - profile update

--- resources/views/frontend/dashboard/layouts/master.blade.php
+++ resources/views/frontend/dashboard/layouts/master.blade.php
@@ -22,6 +22,8 @@
     <link rel="stylesheet" href="{{asset('frontend/css/jquery.classycountdown.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/venobox.min.css')}}">
 
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
     <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
     <link rel="stylesheet" href="{{asset('frontend/css/responsive.css')}}">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
@@ -98,9 +100,20 @@
 <script src="{{asset('frontend/js/venobox.min.js')}}"></script>
 <!--classycountdown js-->
 <script src="{{asset('frontend/js/jquery.classycountdown.js')}}"></script>
+<!--toastr js-->
+<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <!--main/custom js-->
 <script src="{{asset('frontend/js/main.js')}}"></script>
+
+<!--show dynamic validation errorss-->
+<script>
+    @if($errors->any())
+    @foreach($errors->all() as $error)
+    toastr.error("{{$error}}")
+    @endforeach
+    @endif
+</script>
 </body>
 
 </html>
--- routes/vendor.php
+++ routes/vendor.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\VendorProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 /** Vendor Routes */
 Route::get('dashboard', [vendorController::class, 'dashboard'])->name('dashboard');
+Route::get('vendor', [VendorProfileController::class, 'index'])->name('profile');
+Route::put('update', [VendorProfileController::class, 'updateProfile'])->name('profile.update'); // vendor.profile.update
+Route::post('update', [VendorProfileController::class, 'updatePassword'])->name('profile.update.password'); // vendor.profile.update.password
 
